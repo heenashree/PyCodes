4714b8e399913ebee76c282c68fda468542be86e
heenashree <heenashree2010@gmail.com>
2019-07-23 16:45:18 +0530

    PythonCodes OOPs DataAlgorithms

--- Pandas/Data analysis using Pandas.py
+++ Pandas/Data analysis using Pandas.py
@@ -11,7 +11,7 @@ Index = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 # Creating series with predefined index values
 si = pd.Series(Data, Index)
-print(s, si)
+print("Example1", s, si)
 
 #Example2
 # When Data contains Dictionary
@@ -20,7 +20,7 @@ dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 
 # Creating series of Dictionary type
 sd = pd.Series(dictionary)
-print(sd)
+print("Example2", sd)
 
 #Example3
 #DataFrames:
@@ -35,6 +35,7 @@ dict1 ={'a':1, 'b':2, 'c':3, 'd':4}	 # Define Dictionary 1
 dict2 ={'a':5, 'b':6, 'c':7, 'd':8, 'e':9} # Define Dictionary 2
 Data = {'first':dict1, 'second':dict2} # Define Data with dict1 and dict2
 df = pd.DataFrame(Data) # Create DataFrame
+print("Example3", df)
 
 # When data is series
 # Program to create Dataframe of three series
@@ -47,13 +48,18 @@ s3 = pd.Series(['a', 'b', 'c', 'd', 'e'])	 # Define series 3
 
 Data ={'first':s1, 'second':s2, 'third':s3} # Define Data
 dfseries = pd.DataFrame(Data)			 # Create DataFrame
+print("Example4", dfseries)
 
 ## Program to create DataFrame from 2D array
-#IMPORTANT - One constraint has to be maintained while creating DataFrame of 2D arrays – Dimensions of 2D array must be same.
+# IMPORTANT  One constraint has to be maintained while creating DataFrame of 2D arrays - Dimensions of 2D array must be same.
 import pandas as pd # Import Library
 d1 =[[2, 3, 4], [5, 6, 7]] # Define 2d array 1
 d2 =[[2, 4, 8], [1, 3, 9]] # Define 2d array 2
 Data ={'first': d1, 'second': d2} # Define Data
 df2d = pd.DataFrame(Data) # Create DataFrame
+print("Example5", df2d)
+
+
+
 
 
