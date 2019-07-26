5d56915d3292b66e2eced8a5a5d5116ab7a65a05
heenashree <heenashree2010@gmail.com>
2019-07-26 14:57:27 +0530

    PythonCodes OOPs DataAlgorithms

--- Python Codes/XML-YAML/loop over directory.py
+++
@@ -1,5 +0,0 @@
-import os
-directory = '../A/B/'
-for l in os.listdir(directory):
-    if (os.path.isdir(directory + l)):
-        print("Yep, it is directory")
--- Python Codes/XML-YAML/read from a file.py
+++
@@ -1,6 +0,0 @@
-def file_lengthy(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            if 'nttrbsvpa' in l:
-                break
-    return i - 1
