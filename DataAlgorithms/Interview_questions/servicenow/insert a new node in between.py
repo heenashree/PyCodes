9d4600dabd097fec48e7c2b7dbb30573f2a6c6bc
heenashree <heenashree2010@gmail.com>
2019-08-22 18:50:18 +0530

    insert at position

---
+++ DataAlgorithms/Interview_questions/servicenow/insert a new node in between.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def Endinsert(self, newnode):
+        if self.head is None:
+            self.head = newnode
+        else:
+            lastnode =self.head
+            while True:
+                if lastnode.next is None:  # last node
+                    break
+                lastnode = lastnode.next  # we go to next node which is ben
+            lastnode.next = newnode
+
+    def Homeinsert(self,newnode):
+        tempnode = self.head
+        self.head = newnode
+        newnode.next = tempnode
+
+    def InBetweenInsert(self, node, position):
+        currentNode = self.head
+        currentPosition  = 0
+        while True:
+            if currentPosition == position:
+                previousNode.next=node
+                node.next = currentNode
+                break
+            previousNode = currentNode
+            currentNode = currentNode.next
+            currentPosition+=1
+
+
+
+
+    def printlist(self):
+        currenetnode = self.head
+        while currenetnode:
+            print(currenetnode.data)
+            currenetnode = currenetnode.next
+
+
+
+firstnode = Node("J")
+linkedlist = Linkedlist()
+linkedlist.Endinsert(firstnode)
+
+secondnode = Node("J12")
+linkedlist.Endinsert(secondnode)
+
+thirdnode = Node("J22")
+linkedlist.Endinsert(thirdnode)
+
+
+
+homenode = Node("Heena")
+linkedlist.Homeinsert(homenode)
+
+
+
+betweennode = Node("I am in between")
+linkedlist.InBetweenInsert(betweennode, 2)
+linkedlist.printlist()
+
+
+
+
