bdc0ada40f3ca0d4e29bd6bb81e38645479d9361
heenashree <heenashree2010@gmail.com>
2019-10-04 12:47:53 +0530

    check same elements in list

---
+++ Python Codes/miscellanious/password check.py
@@ -0,0 +1,17 @@
+import re
+
+
+def checkio(data: str) -> bool:
+    return True if len(data) >= 10 and any(x.isdigit() for x in data) and any(x.isupper() for x in data) and any(x.islower() for x in data) else False
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
